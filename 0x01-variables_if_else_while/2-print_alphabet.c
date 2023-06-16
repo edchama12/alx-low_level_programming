3170b096ac87abd40b67df9939ec0c5cea7fcfe0
ED-DARRAZ Chaimae <chaimae.ed.darraz12338@gmail.com>
2023-06-16 10:51:57 -0700

    display alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - start point
+ * Return: 0 if success
+ */
+int main(void)
+{
+	int i;
+
+	i = 97;
+	while (i < 123)
+	{
+		putchar(i);
+		i++;
+	}
+	return (0);
+}
