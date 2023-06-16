144032856ac52203cbd7f379cd3e370f79683a19
ED-DARRAZ Chaimae <chaimae.ed.darraz12338@gmail.com>
2023-06-16 11:02:43 -0700

    Lowe and upper case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.>
+/**
+ * main - start point
+ * Return: 0 if succes
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+
+	i = 65;
+	j = 97;
+	while (j < 123)
+	{
+		putcahr(j);
+		j++;
+	}
+	while (i < 100)
+	{
+		putchar(i);
+		i++;
+	}
+	putcahr(10);
+	return (0);
+}
