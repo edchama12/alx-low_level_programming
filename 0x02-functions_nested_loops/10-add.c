34c54f5d015591659335004504911f6753211a97
ED-DARRAZ Chaimae <chaimae.ed.darraz12338@gmail.com>
2023-06-20 16:20:58 -0700

    task of sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <main.h>
+/**
+ * add - make sum of two numbers
+ * @int: the first number
+ * @int; the second number
+ * Return; the result of the sum
+ */
+int add(int, int)
+{
+	return (a + b);
+}
