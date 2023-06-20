fb229bb2b5be55e4b39ebea736bd050dfd0a4234
ED-DARRAZ Chaimae <chaimae.ed.darraz12338@gmail.com>
2023-06-20 16:34:09 -0700

    from n to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_to_98: print from n to 98
+ * @n: the start number
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	for (i = n; i <=98; i++)
+	{
+		if (i == 98)
+			printf("%d/n", i);
+			continue;
+		printf("%d, ", i);
+	}
+}
