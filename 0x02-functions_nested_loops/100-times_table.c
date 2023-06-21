796d1943371a72f6e44c74a40c81c1be46c379d8
ED-DARRAZ Chaimae <chaimae.ed.darraz12338@gmail.com>
2023-06-21 14:18:06 -0700

    done

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,14 +8,14 @@ void print_times_table(int n)
 {
 	int i, j, r;
 
-	if (n <= 15 && n > 0)
+	if (n <= 15 && n >= 0)
 	{
 		for (i = 0; i <= n; i++)
 		{
 			for (j = 0; j <= n; j++)
 			{
 				r = i * j;
-				if ( j == 0)
+				if (j == 0)
 					printf("%d", i * j);
 				else if (r < 10 && j != 0)
 					printf(",   %d", i * j);
