70d61a4bf429c5c36908f65ee78a78c5c0494dd6
ED-DARRAZ Chaimae <chaimae.ed.darraz12338@gmail.com>
2023-06-21 08:22:08 -0700

    fixe error

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,9 +15,9 @@ void times_table(void)
 			if (j == 0)
 				printf("%d", i * j);
 			else if (r < 10 && j != 0)
-				printf(",  %d\n", i * j);
+				printf(",  %d", i * j);
 			else if (r >= 10)
-				printf(", %d,  ", i * j);
+				printf(", %d", i * j);
 		}
 		putchar(10);
 	}
