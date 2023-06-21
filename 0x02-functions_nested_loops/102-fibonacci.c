14b7234222d4a40cb9d574cca0140be7df8e2608
ED-DARRAZ Chaimae <chaimae.ed.darraz12338@gmail.com>
2023-06-21 14:49:05 -0700

    done

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - strat point fibonacci numbers 
+ * Return: 0 if sucess
+ */
+int main(void)
+{
+	int cpt;
+	int n1, n2, fin;
+
+	n1 = 1;
+	n2 = 2;
+	printf("%d, %d", n1, n2);
+	for (cpt = 0; cpt < 48; c++)
+	{
+		fin = n1 + n2;
+		printf(", %d", fin);
+		n1 = n2;
+		n2 = fin;
+	}
+	printf("\n");
+	return (0);
+}
