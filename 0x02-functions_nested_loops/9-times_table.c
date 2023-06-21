dd280082a77a0f46a44d9eb3252a37e772130ff0
ED-DARRAZ Chaimae <chaimae.ed.darraz12338@gmail.com>
2023-06-21 07:42:45 -0700

    times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * times_table - make time table
+ */
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if ( j != 9)
+				printf("%d, ", i * j);
+			else
+				printf("%d\n", i * j);
+		}
+	}
+}
