d546894ef174c35acba2b23d1c3870df96919832
ED-DARRAZ Chaimae <chaimae.ed.darraz12338@gmail.com>
2023-06-22 10:26:04 -0700

    done

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - multiple two intger
+ * @a: first intger
+ * @b: second intger
+ * Return: result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
