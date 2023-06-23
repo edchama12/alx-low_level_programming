cd1f8add505f919ac3af14ab4e4b269c05bb597a
ED-DARRAZ Chaimae <chaimae.ed.darraz12338@gmail.com>
2023-06-23 10:33:37 -0700

    done

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_triangle - write a triangle with #
+ * @size: intger argument
+ */
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	if (size > 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = size - i; j > 0; j--)
+			{
+				putchar(' ');
+			}
+			for (l = 0; l < i; l++)
+			{
+				putchar('#');
+			}
+			if (i == size)
+				continue;
+			putchar(10);
+		}
+	}
+	else
+	{
+		putchar(10);
+	}
+}
