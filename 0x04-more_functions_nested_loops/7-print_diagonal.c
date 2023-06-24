f9229d8cb8225b8b3f61c2e941b8cf1bed4717ec
ED-DARRAZ Chaimae <chaimae.ed.darraz12338@gmail.com>
2023-06-24 08:58:00 -0700

    done

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,22 +12,16 @@ void print_diagonal(int n)
 	{
 		for (i = 0; i < n; i++)
 		{
-			for (j = 0; j < n; j++)
-			{
-				if (j == i)
-				{
-					putchar('\\');
-				}
-				else
-				{
-					putchar(' ');
-				}
-			}
+			for (j = 0; j < i; j++)
+				putchar(' ');
+			putchar('\\');
+
+			if (i == n - 1)
+				continue;
+
 			putchar('\n');
 		}
 	}
-	else
-	{
-		putchar('\n');
-	}
+
+	putchar('\n');
 }
