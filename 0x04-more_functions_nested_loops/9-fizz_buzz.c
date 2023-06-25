abfe87b8ad1bbb86b931e593dab8d87555d488e6
ED-DARRAZ Chaimae <chaimae.ed.darraz12338@gmail.com>
2023-06-25 08:34:12 -0700

    done

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,21 +9,19 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
-		{
-			printf("FizzBuzz ");
-		}
-		else if (i % 5 == 0)
-		{
-			printf("Buzz ");
-		}
-		else if (i % 3 == 0)
-		{
-			printf("Fizz ");
-		}
-		else	
-			printf("%d ", i);
+		if ((i % 3) == 0 && (i % 5) ==0)
+			printf("FizzBuzz");
+		else if ((i % 3) == 0)
+			printf("Fizz");
+		else if ((i % 5) == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+
+		if (i == 100)
+			continue;
+		printf(" ");
 	}
-	putchar(10);
+	printf("\n");
 	return (0);
 }
