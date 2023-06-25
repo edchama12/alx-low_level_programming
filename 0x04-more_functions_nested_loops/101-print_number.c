0bff204f4ff3f7b501d208ea2958b6cc4f7c0394
ED-DARRAZ Chaimae <chaimae.ed.darraz12338@gmail.com>
2023-06-25 08:51:00 -0700

    done

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,12 +10,12 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-		putchar('-');
+		_putchar('-');
 		i = -i;
 	}
 
 	if ((i / 10) > 0)
 		print_number(i / 10);
 
-	putchar((i % 10) + '0');
+	_putchar((i % 10) + '0');
 }
