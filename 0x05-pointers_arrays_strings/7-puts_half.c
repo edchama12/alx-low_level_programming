33c77606b516962fd2df9e5354c76f2e5cf82a26
ED-DARRAZ Chaimae <chaimae.ed.darraz12338@gmail.com>
2023-06-27 11:27:07 -0700

    done

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+/**
+ * puts_half - print half string
+ * @str: string argument
+ */
+void puts_half(char *str)
+{
+	int i, l, HL;
+
+	l = strlen(str);
+	if (l % 2 == 0)
+		HL = l / 2;
+	else
+		HL = (l - 1) / 2;
+	for (i = HL; i <= l; i++)
+	{
+		printf("%c", str[i])
+	}
+	putchar(10);
+}
