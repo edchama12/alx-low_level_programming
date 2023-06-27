4c3eca599b4df1f27c31e3492d8063638826dc47
ED-DARRAZ Chaimae <chaimae.ed.darraz12338@gmail.com>
2023-06-27 07:56:40 -0700

    done

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - lenght of a string
+ * @s: string argument
+ * Return: lenght of the string
+ */
+int _strlen(char *s)
+{
+	int l;
+
+	l = strlen(*s);
+	return (l);
+}
+
