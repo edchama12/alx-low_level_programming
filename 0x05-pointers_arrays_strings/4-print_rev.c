6b300372e20ad09462a3d3e00f12d208de32d76c
ED-DARRAZ Chaimae <chaimae.ed.darraz12338@gmail.com>
2023-06-27 11:04:53 -0700

    done

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+/**
+ * print_rev - write reverse string
+ * @s: string argument
+ */
+void print_rev(char *s)
+{
+	int l, i;
+
+	l = strlen(s);
+	for (i = l; i >= 1; i--)
+	{
+		printf("%c", s[i]);
+	}
+	printf("\n");
+}
---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+/**
+ * rev_string - reverse string
+ * @s: string argument
+ */
+void rev_string(char *s)
+{
+	int l, i, cpt;
+	char *c;
+
+	*c = *s;
+	l = strlen(s);
+	cpt = 0;
+	for (i = l; i >= 1; l--)
+	{
+		s[cpt] = c[i];
+		cpt++;
+	}
+}
+
+
