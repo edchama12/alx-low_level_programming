b7e2dd042851520a95cbfec0a56c035dc506ea67
ED-DARRAZ Chaimae <chaimae.ed.darraz12338@gmail.com>
2023-06-27 16:54:38 -0700

    done

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,12 +10,12 @@ void rev_string(char *s)
 	int l, i, cpt;
 	char *c;
 
-	*c = *s;
+	strcpy(c, s)
 	l = strlen(s);
 	cpt = 0;
 	for (i = l; i >= 1; l--)
 	{
-		s[cpt] = c[i];
+		s[cpt] + = c[i];
 		cpt++;
 	}
 }
