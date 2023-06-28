a70707952bf19eaf5fa18b9f26ce1cb9ce015281
ED-DARRAZ Chaimae <chaimae.ed.darraz12338@gmail.com>
2023-06-28 07:32:00 -0700

    done

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,17 +8,17 @@
  */
 int _atoi(char *s)
 {
-	int s = 1;
+	int sg = 1;
 	unsigned int n = 0;
 
 	while (*s++)
 	{
 		if (*s == '-')
-			s *= -1;
+			sg *= -1;
 		else if (*s >= '0' && *s <= '9')
 			n = (n * 10) + (*s - '0');
 		else if (n > 0)
 			break;
 	}
-	return (n * s);
+	return (n * sg);
 }
