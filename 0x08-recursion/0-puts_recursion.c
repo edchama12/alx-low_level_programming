aeaa3a26465453be62345728b29206fbac04f7b5
ED-DARRAZ Chaimae <chaimae.ed.darraz12338@gmail.com>
2023-07-09 07:57:32 -0700

    done

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * _puts_recursion - write a string
+ * @s: string to write
+ */
+void _puts_recursion(char *s)
+{
+	puts(s);
+	return (0);
+}
