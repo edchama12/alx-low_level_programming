30e9c795356591ff4041c894f5c075f810e76850
ED-DARRAZ Chaimae <chaimae.ed.darraz12338@gmail.com>
2023-07-13 08:54:33 -0700

    done

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * _strlen_recursion - length og string
+ * @s: char pointer argument
+ * Return: length of string
+ */
+int _strlen_recursion(char *s)
+{
+	int l = 0;
+
+	if (s != '\0')
+	{
+		l++;
+		return (s++);
+	}
+	return (l);
+}
