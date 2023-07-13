288eb3a8793ecd0c0e6cc494e5be1d711f87e8c7
ED-DARRAZ Chaimae <chaimae.ed.darraz12338@gmail.com>
2023-07-13 09:17:54 -0700

    done

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -10,10 +10,10 @@ int _strlen_recursion(char *s)
 {
 	int l = 0;
 
-	if (*s != '\0')
+	if (*s)
 	{
 		l++;
-		l = l + _strlen_recursion(s++);
+		l + = _strlen_recursion(s + 1);
 	}
 	return (l);
 }
