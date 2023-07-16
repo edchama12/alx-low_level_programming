d36396de17832626d307fb502ffffb80247e9d4c
ED-DARRAZ Chaimae <chaimae.ed.darraz12338@gmail.com>
2023-07-16 08:37:20 -0700

    done

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - start point
+ * @argc: number of argument line
+ * @argv:array list of argument
+ * Return: 0 if succes
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
