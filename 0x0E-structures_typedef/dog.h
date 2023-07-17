a0d2b962d8f703fd7f430d78e9b36d3725e4152d
ED-DARRAZ Chaimae <chaimae.ed.darraz12338@gmail.com>
2023-07-17 08:27:33 -0700

    done

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,12 @@
+/**
+ * struct dog - struct of dog
+ * @name: char argument
+ * @age: age argument in float
+ * @owner: owner name char type
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
